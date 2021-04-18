2e10ef5e9f59bf31735cbab1f0b67a72ab4beda0
25timezone <whovian_zone@mail.ru>
2021-04-18 20:09:01 +0200

    Finished. Again.

--- DZ2/Aproximation.py
+++ DZ2/Aproximation.py
@@ -15,3 +15,19 @@ def force_approximation(x, u, l, EF):
     N2prime = 1 / l
     N = [N1prime, N2prime]
     return scalar_product(N, u) * EF
+
+# Перемещения
+def per(q1, q2):
+    print("0   ", approximation(0, [q1, q2], 1))
+    print("0.25", approximation(0.25, [q1, q2], 1))
+    print("0.5 ", approximation(0.5, [q1, q2], 1))
+    print("0.75", approximation(0.75, [q1, q2], 1))
+    print("1   ", approximation(1, [q1, q2], 1))
+
+# Силы
+def force(q1, q2, EF):
+    print("0   ", force_approximation(0, [q1, q2], 1, EF))
+    print("0.25", force_approximation(0.25, [q1, q2], 1, EF))
+    print("0.5 ", force_approximation(0.5, [q1, q2], 1, EF))
+    print("0.75", force_approximation(0.75, [q1, q2], 1, EF))
+    print("1   ", force_approximation(1, [q1, q2], 1, EF))
--- DZ2/Finit_elem_meth_dz.py
+++ DZ2/Finit_elem_meth_dz.py
@@ -60,32 +60,13 @@ print('u', q)
 
 # Апроксимации
 
-from Aproximation import approximation
-
-def per(q1, q2):
-    print("0   ", approximation(0, [q1, q2], 1))
-    print("0.25", approximation(0.25, [q1, q2], 1))
-    print("0.5 ", approximation(0.5, [q1, q2], 1))
-    print("0.75", approximation(0.75, [q1, q2], 1))
-    print("1   ", approximation(1, [q1, q2], 1))
-
-from Aproximation import force_approximation
-
-def force(q1, q2, EF):
-    print("0   ", force_approximation(0, [q1, q2], 1, EF))
-    print("0.25", force_approximation(0.25, [q1, q2], 1, EF))
-    print("0.5 ", force_approximation(0.5, [q1, q2], 1, EF))
-    print("0.75", force_approximation(0.75, [q1, q2], 1, EF))
-    print("1   ", force_approximation(1, [q1, q2], 1, EF))
+from Aproximation import per, force
 
 # Перемещения элементов и усилия
+EF_all = [С1, EF1, EF2, EF3]
 
 for i in range(0,4):
     print('Перемещения элемента', i)
     per(q[i],q[i+1])
-
-EF_all = [С1, EF1, EF2, EF3]
-
-for i in range(0,4):
     print('Усилия для элемента', i)
-    force(q[i],q[i+1],EF_all[i])
+    force(q[i], q[i + 1], EF_all[i])
